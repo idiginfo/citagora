295dbdfbd7a665f376d0ce2c730171a5c1642f99
griccardi <griccardi@fsu.edu>
2012-07-29 20:03:42 -0400

    Refactoring and new functionality

---
+++ annotateservices/src/org/idiginfo/annotationmodel/Document.java
@@ -0,0 +1,38 @@
+package org.idiginfo.annotationmodel;
+
+import java.util.Date;
+
+public interface Document {
+
+	public String getId();
+
+	public void setId(String id);
+
+	public String getDate();
+
+	public Date getDateObject();
+
+	public void setDate(Date date);
+
+	public String getName();
+
+	public void setName(String name);
+
+	public String getOwner();
+
+	public void setOwner(String owner);
+
+	public String getType();
+
+	public void setType(String type);
+
+	public Document getParent();
+
+	public void setParent(Document parent);
+
+	public Annotation[] getAnnotations();
+
+	public String getTitle();
+
+	public void setTitle(String title);
+}
