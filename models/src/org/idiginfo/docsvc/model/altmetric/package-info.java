d17182e461c96b13fe61a4a44fb11f37a1bc9557
griccardi <griccardi@fsu.edu>
2012-08-20 17:28:17 -0400

    add package-info for altmetrics model package

---
+++ models/src/org/idiginfo/docsvc/model/altmetric/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Contains classes that have been used to explore the capabilities of JSON tools 
+ * and the altmetric service API
+ * 
+ * The altmetric.com service API supports access to citation and social network reference information
+ * It does not support search for documents. Rather, give a doi or other id for a document, 
+ * the service will provide information about the document
+ */
+package org.idiginfo.docsvc.model.altmetric;
+
