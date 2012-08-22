36c0caab7fb864068cc83ce22f827d7dc75d1be4
griccardi <griccardi@fsu.edu>
2012-08-22 13:48:42 -0400

    added more fields to rdf.MapDocument and extended MapDocuments

--- views/src/main/java/org/idiginfo/docsvc/view/rdf/MapDocument.java
+++ views/src/main/java/org/idiginfo/docsvc/view/rdf/MapDocument.java
@@ -2,21 +2,23 @@ package org.idiginfo.docsvc.view.rdf;
 
 import org.idiginfo.docsvc.model.model.Document;
 import org.idiginfo.docsvc.view.rdf.vocabulary.BIBO;
-import org.idiginfo.docsvc.view.rdf.vocabulary.DublinCore;
 import org.idiginfo.docsvc.view.rdf.vocabulary.RdfUtilities;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.DCTerms;
 
 /**
  * Tools for mapping from model.Document to/from RDF
+ * 
  * @author griccardi
- *
+ * 
  */
 public class MapDocument {
 
 	/**
 	 * Add the document to the model
+	 * 
 	 * @param model
 	 * @param document
 	 * @return
@@ -27,8 +29,20 @@ public class MapDocument {
 
 		RdfUtilities.addProperty(model, docResource, BIBO.authorList,
 				document.getAuthors());
-		RdfUtilities.addProperty(model, docResource, DublinCore.title, document.getTitle());
-		RdfUtilities.addProperty(model, docResource, BIBO.doi, document.getDoi());
+		RdfUtilities.addProperty(model, docResource, BIBO.doi,
+				document.getDoi());
+		RdfUtilities.addProperty(model, docResource, BIBO.identifier,
+				document.getId());
+		RdfUtilities.addProperty(model, docResource, DCTerms.date,
+				document.getDate());
+		RdfUtilities.addProperty(model, docResource, DCTerms.source,
+				document.getName());
+		RdfUtilities.addProperty(model, docResource, BIBO.owner,
+				document.getOwner());
+		RdfUtilities.addProperty(model, docResource, DCTerms.type,
+				document.getType());
+		RdfUtilities.addProperty(model, docResource, DCTerms.title,
+				document.getTitle());
 		return model;
 	}
 }
--- views/src/main/java/org/idiginfo/docsvc/view/rdf/MapDocuments.java
+++ views/src/main/java/org/idiginfo/docsvc/view/rdf/MapDocuments.java
@@ -9,21 +9,26 @@ import com.hp.hpl.jena.rdf.model.Model;
 
 /**
  * Tools for mapping from model.Document to/from RDF
+ * 
  * @author griccardi
- *
+ * 
  */
 public class MapDocuments {
 
 	MapDocument mapDocument = new MapDocument();
+
 	/**
 	 * Add the documents to the model
+	 * 
 	 * @param model
 	 * @param documents
 	 * @return
 	 */
 	public Model addDocuments(Model model, Documents documents) {
+		if (documents == null)
+			return model;
 		Iterator<Document> docs = documents.iterator();
-		while (docs.hasNext()){
+		while (docs.hasNext()) {
 			Document document = docs.next();
 			mapDocument.addDocument(model, document);
 		}
