2ceabe0f47be0eaab463342be97e5c4d5418ca23
griccardi <griccardi@fsu.edu>
2012-08-31 08:44:08 -0400

    some package-info files

---
+++ models/src/org/idiginfo/docsvc/svcapi/annotate/model/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes in this package may be used to query the a.nnotate service API 
+ * and return POJO objects that closely match the json objects returned by that API
+ */
+package org.idiginfo.docsvc.svcapi.annotate.model;
+
