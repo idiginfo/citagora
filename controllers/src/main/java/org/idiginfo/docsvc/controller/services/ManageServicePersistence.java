328288f99a9a46f9e294c626a63589f8281ba358
griccardi <griccardi@fsu.edu>
2012-10-31 14:09:35 -0400

    repositories: clean up of some extra files

---
+++ controllers/src/main/java/org/idiginfo/docsvc/controller/services/ManageServicePersistence.java
@@ -0,0 +1,11 @@
+package org.idiginfo.docsvc.controller.services;
+
+/**
+ * Controller class to move objects from service requests to citagora repository
+ * 
+ * @author griccardi
+ *
+ */
+public class ManageServicePersistence {
+
+}
---
+++ repositories/src/org/idiginfo/docsvc/jpa/citagora/ContainerImpl_.java
@@ -0,0 +1,15 @@
+package org.idiginfo.docsvc.jpa.citagora;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2012-10-31T13:57:42.413-0400")
+@StaticMetamodel(ContainerImpl.class)
+public class ContainerImpl_ extends CitagoraObjectImpl_ {
+	public static volatile SingularAttribute<ContainerImpl, ReferenceImpl> isAbout;
+	public static volatile ListAttribute<ContainerImpl, ReviewImpl> reviews;
+	public static volatile ListAttribute<ContainerImpl, TagImpl> tags;
+	public static volatile ListAttribute<ContainerImpl, CommentImpl> comments;
+}
