d4fe2ac9c3ec7c523a0968ecae3520caba7292c0
griccardi <griccardi@fsu.edu>
2012-11-19 10:18:42 -0500

    views: added support for Citagora Services API

--- views/src/main/java/org/idiginfo/docsvc/view/RdfWriter.java
+++ views/src/main/java/org/idiginfo/docsvc/view/RdfWriter.java
@@ -1,10 +1,13 @@
 package org.idiginfo.docsvc.view;
 
 import java.io.StringWriter;
+import java.util.List;
 
 import org.idiginfo.docsvc.model.apisvc.Document;
 import org.idiginfo.docsvc.model.apisvc.Documents;
+import org.idiginfo.docsvc.model.citagora.CitagoraObject;
 import org.idiginfo.docsvc.model.citagora.Container;
+import org.idiginfo.docsvc.model.citagora.UriObject;
 import org.idiginfo.docsvc.view.rdf.BiboModel;
 import org.idiginfo.docsvc.view.rdf.MapDocument;
 import org.idiginfo.docsvc.view.rdf.MapDocuments;
@@ -15,57 +18,71 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 public class RdfWriter implements ObjectWriter {
-	BiboModel biboModel = new BiboModel();
-	Model model = biboModel.createSample();
+    BiboModel biboModel = new BiboModel();
+    Model model = biboModel.createSample();
 
-	static {
-		RIOT.init();// initialize the I/O tools
+    static {
+	RIOT.init();// initialize the I/O tools
+    }
+
+    public static String writeContainer(Container document) {
+	MapCitagoraObject mapper = new MapCitagoraObject();
+	mapper.addContainer(document, -1);
+	Model model = mapper.getModel();
+	return null;
+    }
+
+    @Override
+    public String writeDocument(Document document) {
+	Model model = ModelFactory.createDefaultModel();
+	MapDocument mapDocument = new MapDocument();
+	model = mapDocument.addDocument(model, document);
+	return writeModel(model);
+    }
+
+    @Override
+    public String writeDocuments(Documents documents) {
+	Model model = ModelFactory.createDefaultModel();
+	MapDocuments mapDocuments = new MapDocuments();
+	model = mapDocuments.addDocuments(model, documents);
+	return writeModel(model);
+    }
+
+    private String writeModel(Model model) {
+	return writeModel(model, "RDF/XML");
+	// return writeModel(model, "RDF/XML");
+	// return writeModel(model, "Turtle");
+    }
+
+    private String writeModel(Model model, String version) {
+	StringWriter out = new StringWriter();
+	model.write(out, version);
+	return out.toString();
+    }
+
+    @Override
+    public String write(Object objects) {
+	if (objects instanceof Documents) {
+	    return writeDocuments((Documents) objects);
 	}
-
-	public static String writeContainer(Container document) {
-		MapCitagoraObject mapper = new MapCitagoraObject();
-		mapper.addContainer(document, -1);
-		Model model = mapper.getModel();
-		return null;
+	if (objects instanceof Document) {
+	    return writeDocument((Document) objects);
 	}
-
-	@Override
-	public String writeDocument(Document document) {
-		Model model = ModelFactory.createDefaultModel();
-		MapDocument mapDocument = new MapDocument();
-		model = mapDocument.addDocument(model, document);
-		return writeModel(model);
+	if (objects instanceof List) {
+	    return writeCitagora((List<?>) objects, null);
 	}
+	return null;
+    }
 
-	@Override
-	public String writeDocuments(Documents documents) {
-		Model model = ModelFactory.createDefaultModel();
-		MapDocuments mapDocuments = new MapDocuments();
-		model = mapDocuments.addDocuments(model, documents);
-		return writeModel(model);
+    private String writeCitagora(List<?> objects, String version) {
+	MapCitagoraObject mapper = new MapCitagoraObject();
+	StringWriter out = new StringWriter();
+	for (Object obj : objects) {
+	    if (obj instanceof CitagoraObject) {
+		mapper.add((UriObject) obj, -1);
+	    }
 	}
-
-	private String writeModel(Model model) {
-		return writeModel(model, "RDF/XML");
-		// return writeModel(model, "RDF/XML");
-		// return writeModel(model, "Turtle");
-	}
-
-	private String writeModel(Model model, String version) {
-		StringWriter out = new StringWriter();
-		model.write(out, version);
-		return out.toString();
-	}
-
-	@Override
-	public String write(Object objects) {
-		if (objects instanceof Documents) {
-			return writeDocuments((Documents) objects);
-		}
-		if (objects instanceof Document) {
-			return writeDocument((Document) objects);
-		}
-		return null;
-	}
-
+	mapper.getModel().write(out, version);
+	return out.toString();
+    }
 }
