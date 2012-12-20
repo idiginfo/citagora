812ff5fc406ce1b9a86c44a18561492f1cd50844
griccardi <griccardi@fsu.edu>
2012-12-20 08:25:14 -0500

    repositories: update to Impl_ (derived) classes

--- repositories/src/org/idiginfo/docsvc/jpa/citagora/AnnotationImpl_.java
+++ repositories/src/org/idiginfo/docsvc/jpa/citagora/AnnotationImpl_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2012-12-19T13:21:46.690-0500")
+@Generated(value="Dali", date="2012-12-20T08:19:18.498-0500")
 @StaticMetamodel(AnnotationImpl.class)
 public class AnnotationImpl_ extends CitagoraObjectImpl_ {
 	public static volatile SingularAttribute<AnnotationImpl, PersonImpl> annotator;
@@ -12,4 +12,5 @@ public class AnnotationImpl_ extends CitagoraObjectImpl_ {
 	public static volatile SingularAttribute<AnnotationImpl, String> modelVersion;
 	public static volatile SingularAttribute<AnnotationImpl, String> specifier;
 	public static volatile SingularAttribute<AnnotationImpl, String> tags;
+	public static volatile SingularAttribute<AnnotationImpl, String> context;
 }
--- repositories/src/org/idiginfo/docsvc/jpa/citagora/ReplyImpl_.java
+++ repositories/src/org/idiginfo/docsvc/jpa/citagora/ReplyImpl_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2012-09-26T14:06:22.894-0400")
+@Generated(value="Dali", date="2012-12-20T08:14:22.645-0500")
 @StaticMetamodel(ReplyImpl.class)
 public class ReplyImpl_ extends CommentImpl_ {
 	public static volatile SingularAttribute<ReplyImpl, CommentImpl> replyTarget;
