12d66293bb33fb77d51a653b9848a1cfd44392da
griccardi <griccardi@fsu.edu>
2012-12-20 08:28:12 -0500

    models: improvements to svcapi.msrc classes

---
+++ models/src/org/idiginfo/docsvc/svcapi/msrc/MsrcAnnotationRef.java
@@ -0,0 +1,8 @@
+package org.idiginfo.docsvc.svcapi.msrc;
+
+public class MsrcAnnotationRef {
+
+    public String c;
+    public String d;
+
+}
---
+++ models/src/org/idiginfo/docsvc/svcapi/msrc/MsrcDocumentResult.java
@@ -0,0 +1,31 @@
+package org.idiginfo.docsvc.svcapi.msrc;
+
+import java.util.List;
+import java.util.Map;
+
+import org.idiginfo.docsvc.model.apisvc.Documents;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Class to map results of Altmetric API calls
+ * 
+ * @author griccardi
+ * 
+ */
+public class MsrcDocumentResult extends MsrcResult {
+
+   @SerializedName("document_ids")
+    List<Integer> documentIds;
+    
+    public List<Integer> getDocumentIds(){
+	return documentIds;
+    }
+
+    public Documents getDocuments() {
+	// TODO Auto-generated method stub
+	
+	return null; // documents.values();
+    }
+    
+}
