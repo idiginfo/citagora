6fd95982806eb17a8c7f0c1c7f5da8ec79a1f57e
griccardi <griccardi@fsu.edu>
2013-03-11 15:58:49 -0400

    models: moved package crossref from org.idiginfo.models to correct place: org.idiginfo.svcapi

---
+++ models/src/org/idiginfo/docsvc/svcapi/crossref/CrossrefApiParams.java
@@ -0,0 +1,22 @@
+package org.idiginfo.docsvc.svcapi.crossref;
+
+import org.idiginfo.docsvc.model.apisvc.ApiParams;
+import org.idiginfo.docsvc.model.apisvc.BaseApiParams;
+
+public class CrossrefApiParams extends BaseApiParams {
+
+    public static final String API_URL = "http://search.labs.crossref.org/";
+    public static final String SOURCE = "crossref";
+
+    // ?q=joiner&header=true
+
+    public CrossrefApiParams(ApiParams apiParams) {
+	if (apiParams == null)
+	    return;
+	setSearchTerms(apiParams.getSearchTerms());
+    }
+
+    public CrossrefApiParams() {
+    }
+    
+}
---
+++ models/src/org/idiginfo/docsvc/svcapi/crossref/CrossrefUrl.java
@@ -0,0 +1,90 @@
+package org.idiginfo.docsvc.svcapi.crossref;
+
+import org.idiginfo.docsvc.model.apisvc.ApiParams;
+
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.util.Key;
+import com.google.gson.JsonElement;
+
+public class CrossrefUrl extends GenericUrl {
+
+    public CrossrefUrl(String collection, String resultFormat) {
+	super(CrossrefApiParams.API_URL + collection);
+	if (collection.equals("dois")){
+	    header = true;
+	}
+	// apiKey = CrossrefApiParams.API_KEY;
+    }
+
+    // sample URL
+    // http://api.nature.com/content/opensearch/request?queryType=searchTerms&query=darwin+lamarck
+    // &api_key=<API key string here>
+
+    // parameters
+    // facetLimit, httpAccept, maximumRecords, operation, queryType,
+    // recordPacking, recordSchema, sortKeys, startRecord, version
+
+    // CQL query sample
+
+    // http://api.nature.com/content/opensearch/request?queryType=cql
+    // &query=cql.keywords+any+darwin+OR+cql.keywords+any+lamarck
+    // &httpAccept=application/json
+    // &api_key=<API key string here>
+
+    public CrossrefUrl(String collection, String resultFormat, String function,
+	    ApiParams params) {
+	this(collection, resultFormat);
+	queryString = params.getSearchTerms();
+    }
+
+    @Key("q")
+    protected String queryString = null;
+    @Key
+    String sort;
+    @Key
+    Integer rows;
+    @Key
+    Integer page;
+    // facets
+    // science category, year of publication, publication type, open access
+    // status and publication name.
+    @Key
+    Integer year;
+    // not sure of parameter names
+    @Key
+    String type;
+    @Key
+    String status;
+    @Key
+    String name;
+    @Key
+    Boolean header = null;
+
+    /**
+     * Get the URL ready for execution
+     */
+    public void prepare() {
+
+    }
+
+    public static boolean isError(String content) {
+	if (content.startsWith("XXX")) {
+	    return true;
+	}
+	return false;
+    }
+
+    // protected void mapParams(CrossrefApiParams params) {
+    // if (params == null)
+    // return;
+    // // TODO finish method
+    // }
+
+    public static boolean isError(JsonElement json) {
+	if (json == null)
+	    return true;
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+}
