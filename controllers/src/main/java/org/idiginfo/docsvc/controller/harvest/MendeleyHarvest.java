0f44ed786f8d26f45ed0d2f150e1e2a864aa23f2
sflager <sflager@fsu.edu>
2013-03-20 11:48:48 -0400

    controllers: Mendeley persistence

--- controllers/src/main/java/org/idiginfo/docsvc/controller/harvest/MendeleyHarvest.java
+++ controllers/src/main/java/org/idiginfo/docsvc/controller/harvest/MendeleyHarvest.java
@@ -26,7 +26,7 @@ public class MendeleyHarvest {
     final static String FILE_DIR = "c:/dev/split/mendeleyTitle/";
     final static String FILE_PREFIX = FILE_DIR + "suicide_";
     private final static int MAX_PER_PAGE = 20;
-    private static final int FIRST_PAGE = 1;
+    private static final int FIRST_PAGE = 773;
 
     @SuppressWarnings("unused")
     private static void run(String[] args) {
@@ -60,7 +60,9 @@ public class MendeleyHarvest {
 		+ totalPages + " per file: " + numPerFile);
 	params.setNumResults(numPerFile);
 	for (int pageNum = FIRST_PAGE; pageNum < totalPages; pageNum++) {
-		params.setFirstResult(pageNum-1);
+		int resultPage = pageNum - 1;
+		System.out.println("Now on page number: " + resultPage);
+		params.setFirstResult(resultPage);
 		MendeleyResult result = service.getMendeleyResult("search", keywords, params);
 		List<MendeleyHeader> headers = result.getHeaders();
 		for (int i = 0; i < headers.size(); i++) {
@@ -120,6 +122,3 @@ public class MendeleyHarvest {
 	});
     }
 }
-
-// http://annotate.msrc.fsu.edu/php/listUsers.php?api-auth=yKOfIUFmwDxk21FWkn2X0Ets9fY%3D&api-requesttime=1343244291737&api-user=casey.mclaughlin@cci.fsu.edu&api_key=giqfrstIk9b6CddDL3ogGTUac6Lr3II9
-// http://annotate.msrc.fsu.edu/php/listUsers.php?api-user=casey.mclaughlin%40cci.fsu.edu&api-requesttime=1343244209&api-auth=43F6TiOpvCVhDs4CnNRZHvvPMa0%3D&api-annotateuser=casey.mclaughlin%40cci.fsu.edu
