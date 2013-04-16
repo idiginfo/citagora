ea97f663fd684e6efc518cc775d73e2a3feb0498
griccardi <griccardi@fsu.edu>
2013-04-16 09:54:06 -0400

    repositories: update to derived HarvestResultImpl_ file

--- repositories/src/org/idiginfo/docsvc/jpa/citagora/HarvestResultImpl_.java
+++ repositories/src/org/idiginfo/docsvc/jpa/citagora/HarvestResultImpl_.java
@@ -5,16 +5,16 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2013-04-15T09:15:04.798-0400")
+@Generated(value="Dali", date="2013-04-16T09:44:56.935-0400")
 @StaticMetamodel(HarvestResultImpl.class)
 public class HarvestResultImpl_ {
 	public static volatile SingularAttribute<HarvestResultImpl, Integer> myId;
 	public static volatile SingularAttribute<HarvestResultImpl, String> source;
 	public static volatile SingularAttribute<HarvestResultImpl, Date> created;
 	public static volatile SingularAttribute<HarvestResultImpl, Date> updated;
+	public static volatile SingularAttribute<HarvestResultImpl, String> identifier;
 	public static volatile SingularAttribute<HarvestResultImpl, ReferenceImpl> reference;
 	public static volatile SingularAttribute<HarvestResultImpl, String> description;
 	public static volatile SingularAttribute<HarvestResultImpl, Boolean> success;
 	public static volatile SingularAttribute<HarvestResultImpl, String> uri;
-	public static volatile SingularAttribute<HarvestResultImpl, String> identifier;
 }
