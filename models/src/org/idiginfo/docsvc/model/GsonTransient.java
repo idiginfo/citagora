334a63af2fc29d4447c1afe58376b4a5b4437857
griccardi <griccardi@fsu.edu>
2014-01-30 09:28:16 -0500

    models: added documentation to GsonTransient

--- models/src/org/idiginfo/docsvc/model/GsonTransient.java
+++ models/src/org/idiginfo/docsvc/model/GsonTransient.java
@@ -6,7 +6,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Interface to implement control GSON to JSON
+ * Interface to implement control of transience for GSON writer This interface
+ * allows fields to be marked as transient for output to JSON without being
+ * transient to the persistence model.
+ * 
+ * Used in org.idiginfo.docsvc.jpa.citagora classes that have fields that are
+ * stored in the database but not included in JSON output.
+ * 
+ * See also class org.idiginfo.docsvc.view.MyExclusionStrategy which is used in
+ * classorg.idiginfo.docsvc.view.JsonWriter
  * 
  * @author griccardi
  * 
